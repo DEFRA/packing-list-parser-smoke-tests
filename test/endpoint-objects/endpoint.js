713884d442fd2df0363683915efad5a1efa4d0d4
John Fox <163529520+JohnFoxCap@users.noreply.github.com>
2025-11-18 17:54:09 +0000

    Added api logging (#3)

--- test/endpoint-objects/endpoint.js
+++ test/endpoint-objects/endpoint.js
@@ -1,11 +1,31 @@
+import allureReporter from '@wdio/allure-reporter'
+
 class Endpoint {
   constructor(baseUrl) {
     this.baseUrl = baseUrl
   }
 
-  get(path = '') {
+  async get(path = '') {
     const url = `${process.env.baseEndpointUrl}/${this.baseUrl}/${path}`
-    return fetch(url)
+    allureReporter.addStep(`GET ${url}`)
+
+    const response = await fetch(url)
+    const data = await response.json()
+    allureReporter.addAttachment(
+      'Response',
+      JSON.stringify(data, null, 2),
+      'application/json'
+    )
+    allureReporter.addAttachment(
+      'Status',
+      response.status.toString(),
+      'text/plain'
+    )
+
+    return {
+      status: response.status,
+      data
+    }
   }
 }
 
