5cf44d42238535ec523f30b6f637db51ccd7a141
John Fox <john.fox@capgemini.com>
2026-03-18 16:43:11 +0000

    Added more test coverage for dev

--- test/utilities/config.js
+++ test/utilities/config.js
@@ -1,3 +1,5 @@
-export const baseUrl = process.env.packingListBaseUrl || ''
-export const defaultEstablishmentId = process.env.defaultEstablishmentId || ''
+export const baseUrl = process.env.packingListBaseUrl || undefined
+export const defaultEstablishmentId =
+  process.env.defaultEstablishmentId || undefined
 export const environmentToTest = process.env.ENVIRONMENT || undefined
+export const apiKey = process.env.APIKEY || undefined
