49e38fd81197f5717bc0a1c2884afe25e0fc0b74
Shirisha Kukkala <siri.kukkala@capgemini.com>
2026-07-01 12:13:08 +0100

    Pushing Asda4 fixes

--- test/utilities/config.js
+++ test/utilities/config.js
@@ -4,4 +4,4 @@ export const baseUrl =
 export const defaultEstablishmentId =
   process.env.defaultEstablishmentId || '30614e2b-b895-ee11-be37-000d3aba36b2'
 export const environmentToTest = process.env.ENVIRONMENT || undefined
-export const apiKey = process.env.APIKEY || 'oaHZFpqX1kvHD15s3hKlj6N2X42p841I'
+export const apiKey = process.env.APIKEY || 'BolUaOrkE3Ssjc0nHY4cxr5Log2mpKWr'
