86e5ede2246ea5f4dff68ec70104f1e2abe95a64
Andy Carter <181949839+Andy-Carter-Capgemini@users.noreply.github.com>
2026-07-27 13:30:58 +0100

    Set autocrlf

--- test/utilities/config.js
+++ test/utilities/config.js
@@ -1,7 +1,7 @@
 export const baseUrl =
-  process.env.packingListBaseUrl ||
-  'https://sndexpinfsto002.blob.core.windows.net/application-forms'
+  process.env.packingListBaseUrl ||
+  'https://sndexpinfsto002.blob.core.windows.net/application-forms'
 export const defaultEstablishmentId =
-  process.env.defaultEstablishmentId || '30614e2b-b895-ee11-be37-000d3aba36b2'
+  process.env.defaultEstablishmentId || '30614e2b-b895-ee11-be37-000d3aba36b2'
 export const environmentToTest = process.env.ENVIRONMENT || 'test'
 export const apiKey = process.env.apiKey || null
